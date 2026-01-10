e35cb1167d07673dfb0c05d5e699392c175e6aa4
Rohit Jire <rohitjire55@gmail.com>
2026-01-10 13:06:02 +0100

    initial commit

---
+++ src/main/java/com/project/event_ticket/EventTicketApplication.java
@@ -0,0 +1,13 @@
+package com.project.event_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/event_ticket/EventTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.event_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
