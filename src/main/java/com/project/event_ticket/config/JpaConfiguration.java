c086317b875628ac3e374086a5d35a6187ca0b53
Rohit Jire <rohitjire55@gmail.com>
2026-01-13 22:21:20 +0100

    added jpa auditing config

---
+++ src/main/java/com/project/event_ticket/config/JpaConfiguration.java
@@ -0,0 +1,12 @@
+package com.project.event_ticket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfiguration {
+
+
+
+}
