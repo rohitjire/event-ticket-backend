d9857e3971d006f623ae27e4fdab03efc80543b9
Rohit Jire <rohitjire55@gmail.com>
2026-01-13 21:38:34 +0100

    added user authentication filter

---
+++ src/main/java/com/project/event_ticket/filters/UserProvisioningFilter.java
@@ -0,0 +1,48 @@
+package com.project.event_ticket.filters;
+
+import com.project.event_ticket.domain.User;
+import com.project.event_ticket.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Component
+@RequiredArgsConstructor
+public class UserProvisioningFilter extends OncePerRequestFilter {
+
+    private final UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null
+                && authentication.getPrincipal() instanceof Jwt jwt
+                && authentication.isAuthenticated()) {
+            UUID keycloadkId = UUID.fromString(jwt.getSubject());
+
+            if (!userRepository.existsById(keycloadkId)) {
+                User newUser = new User();
+                newUser.setId(keycloadkId);
+                newUser.setName(jwt.getClaimAsString("preferred_username"));
+                newUser.setEmail(jwt.getClaimAsString("email"));
+                userRepository.save(newUser);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/com/project/event_ticket/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.project.event_ticket.repository;
+
+import com.project.event_ticket.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+
+}
